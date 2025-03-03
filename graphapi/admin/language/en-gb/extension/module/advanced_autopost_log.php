eafe86ab206b1ffdb3478499bbaad8c91e37c030
Alexander Mirvis <45396441+LynxGeekNYC@users.noreply.github.com>
2025-03-03 09:57:20 -0500

    Create advanced_autopost_log.php

---
+++ graphapi/admin/language/en-gb/extension/module/advanced_autopost_log.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title'] = 'AutoPost Logs';
+
+// Text
+$_['text_list']     = 'AutoPost Log List';
+$_['text_success']  = 'Success: You have cleared logs!';
+
+// Column
+$_['column_product_id'] = 'Product ID';
+$_['column_platform']   = 'Platform';
+$_['column_status']     = 'Status';
+$_['column_message']    = 'Message';
+$_['column_date_added'] = 'Date Added';
+
+// Button
+$_['button_clear_log']  = 'Clear Log';
