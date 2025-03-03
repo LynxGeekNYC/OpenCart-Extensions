8eef9e5267c1f7def2634a3b4abf2cf617b257be
Alexander Mirvis <45396441+LynxGeekNYC@users.noreply.github.com>
2025-03-03 09:56:31 -0500

    Create advanced_autopost.php

---
+++ graphapi/admin/language/en-gb/extension/module/advanced_autopost.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title'] = 'Advanced AutoPost: Facebook & Instagram';
+
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success']   = 'Success: You have modified Advanced AutoPost settings!';
+$_['text_edit']      = 'Edit Advanced AutoPost Settings';
+$_['text_log']       = 'View Post Logs';
+
+// Entry
+$_['entry_status']               = 'Module Status';
+$_['entry_auto_mode']            = 'Auto Posting Mode';
+$_['entry_facebook_token']       = 'Facebook Page Access Token';
+$_['entry_instagram_token']      = 'Instagram Access Token';
+$_['entry_instagram_user_id']    = 'Instagram Business User ID';
+$_['entry_post_template']        = 'Post Caption Template';
+$_['entry_post_images']          = 'Post Multiple Images?';
+$_['entry_manual_button']        = 'Manual "Post Now" Button';
+$_['entry_save']                 = 'Save';
+
+// Help
+$_['help_auto_mode']             = 'If enabled, posts are automatically made on add/edit product. If disabled, you can use the "Post Now" button.';
+$_['help_post_template']         = 'Use placeholders: {product_name}, {product_url}, {store_name}.';
+$_['help_post_images']           = 'If enabled, all images associated with the product will be posted as a carousel on Instagram and a multi-photo post on Facebook (if the API supports it).';
+
+// Error
+$_['error_permission']           = 'Warning: You do not have permission to modify Advanced AutoPost!';
